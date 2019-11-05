683f64920550b9cbcb3d4196c6b132e147b06bdc
Danny <mezenov.201521@gmail.com>
2019-11-05 17:46:25 +0300

    Added turn on/off AI possib

--- app/src/main/java/com/example/simplevoiceassistant/MainActivity.java
+++ app/src/main/java/com/example/simplevoiceassistant/MainActivity.java
@@ -16,7 +16,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText questionText;
     private Button sendButton;
     private TextToSpeech tts;
-
+    private Button turnButton;
+    boolean isActive = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,11 +25,26 @@ public class MainActivity extends AppCompatActivity {
         chatView = findViewById(R.id.chatView);
         questionText = findViewById(R.id.questionText);
         sendButton = findViewById(R.id.sendButton);
+        turnButton = findViewById(R.id.turnButton);
+
         tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
                 if (status != TextToSpeech.ERROR){
                     tts.setLanguage(new Locale("ru"));
+                }
+            }
+        });
+        turnButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(isActive){
+                    isActive = false;
+                    turnButton.setText("Включить Искуственный Интеллект");
+                }
+                else{
+                    isActive = true;
+                    turnButton.setText("Выключить Искуственный Интеллект");
                 }
             }
         });
@@ -39,13 +55,17 @@ public class MainActivity extends AppCompatActivity {
                 String question = questionText.getText().toString();
                 questionText.setText(""); //Сброс поля
                 //Здесь должен быть ИИ для ответов//
-
-                String answer = AI.getAnswer(question);//TODO
-                //
-                chatView.append("\n<<" + question); //Вывод вопроса на экран
-                tts.speak(answer, TextToSpeech.QUEUE_FLUSH, null);//Воспроизведение ответа
-                chatView.append("\n>>" + answer);//Вывод ответа на экран
+                if (isActive){
+                    String answer = AI.getAnswer(question);//TODO
+                    chatView.append("\n<<" + question); //Вывод вопроса на экран
+                    tts.speak(answer, TextToSpeech.QUEUE_FLUSH, null);//Воспроизведение ответа
+                    chatView.append("\n>>" + answer);//Вывод ответа на экран
+                }
+                else{
+                    chatView.append("\n>>Вы не активировали ИИ");
+                }
             }
         });
+
     }
 }
